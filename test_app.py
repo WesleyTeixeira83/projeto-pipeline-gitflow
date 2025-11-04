5026e6b03afb831c80b3d8234b8a19664574f6e8
Wesley Gabriel T. Aragão <wesleygabrielteixeiradearagao@gmail.com>
2025-11-04 08:58:14 -0300

    fix: corrige

--- test_app.py
+++ test_app.py
@@ -2,7 +2,7 @@ import unittest
 
 class testapp(unittest.TestCase):
     def test_placeholder(self):
-        self.assertrue(True)
+        self.asserTrue(True)
 
     if __name__ =="__main__":
         unittest.main()
